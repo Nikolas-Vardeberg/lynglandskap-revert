f4c4639f53d4c9d37c0303c43ff5424b6bacaa27
niklusrook <nikolas.vardeberg1@gmail.com>
2024-09-11 14:13:35 +0200

    feat: closeMenu onclick function

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import * as React from 'react'
 import { MdArrowRight, MdOutlineSearch, MdSearch } from 'react-icons/md'
-import { IoIosMenu } from 'react-icons/io'
+import { IoIosMenu, IoMdClose } from 'react-icons/io'
 import { FaArrowRightLong } from "react-icons/fa6";
 
 import { NavLogo } from './NavLogo'
@@ -44,6 +44,11 @@ export const Navigation = ({ menu, blog }: Props) => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+    setIsSearchOpen(false);
+  };
+
   return (
     <header className="w-full flex-col border-b  mx-auto bg-green-dark">
       <div className="container flex flex-row py-5 items-center justify-between px-8 lg:px-0 mx-auto">
@@ -51,14 +56,20 @@ export const Navigation = ({ menu, blog }: Props) => {
                 Lyng Landskap
             </a>
             <div className="flex flex-row space-x-2">
-                <div onClick={handleSearchToggle} id="search-toggle" className="flex border border-green-tan text-green-tan min-w-fit min-h-fit h-0 px-3 md:px-6 py-4 md:py-5 justify-center rounded-full hover:bg-green-tan hover:text-black duration-300 ">
+                <div onClick={handleSearchToggle} 
+                   className={`flex border ${isSearchOpen ? "bg-green-tan text-black" : " text-green-tan border-green-tan"} min-w-fit min-h-fit h-0 px-4 md:px-8 py-3
+                   md:py-5 justify-center items-center rounded-full hover:bg-green-tan hover:text-black duration-300`}
+                >
                     <div className="flex justify-center items-center">
-                        <span className="flex text-sm md:text-base items-center space-x-1"><MdOutlineSearch className="mr-2 text-lg items-center justify-center flex mx-auto"/>Søk</span>
+                        {isSearchOpen ? <IoMdClose className='mr-2 items-center text-lg justify-center flex mx-auto' /> : <MdOutlineSearch className='mr-2 text-lg items-center justify-center flex-auto' />}Søk
                     </div>
                 </div>
-                <div onClick={handleMenuToggle} className="flex border border-green-tan text-green-tan min-w-fit min-h-fit h-0 px-4 md:px-8 py-3 md:py-5 justify-center items-center rounded-full hover:bg-green-tan hover:text-black duration-300 ">
-                    <div className="flex justify-center items-center">
-                        <span className="flex text-sm md:text-base items-center justify-center"><IoIosMenu className="mr-2 items-center text-lg justify-center flex mx-auto"/>Meny</span>
+                <div onClick={handleMenuToggle} 
+                  className={`flex border ${isMenuOpen ? "bg-green-tan text-black" : " text-green-tan border-green-tan"} min-w-fit min-h-fit h-0 px-4 md:px-8 py-3
+                  md:py-5 justify-center items-center rounded-full hover:bg-green-tan hover:text-black duration-300`}
+                >
+                    <div className="flex justify-center items-center transition-all duration-100">
+                      {isMenuOpen ? <IoMdClose className="mr-2 items-center text-lg justify-center flex mx-auto" /> : <IoIosMenu className="mr-2 items-center text-lg justify-center flex mx-auto" />}Meny
                     </div>
                 </div>
 
@@ -81,20 +92,20 @@ export const Navigation = ({ menu, blog }: Props) => {
         <ul className="container grid grid-cols-1 px-4 lg:grid-cols-3 items-start md:px-0 text-base justify-between w-full mx-auto">
           <li className='flex justify-start items-start order-3 lg:order-1 flex-col space-y-4'>
             {menu?.map((item) => (
-              <Link href={item.url} className='flex justify-center items-center text-black' key={item._key}>
+              <Link onClick={closeMenu} href={item.url} className='flex justify-center items-center text-black' key={item._key}>
                 <span className='flex text-lg items-center justify-center'>{item.title} &rarr;</span>
               </Link>
             ))}
           </li>
           <li className='flex justify-start items-start flex-col space-y-4 order-3 lg:order-2'>
             {blog?.map((item) => (
-              <Link href={item.url} className='flex justify-center items-center text-black' key={item._key}>
+              <Link onClick={closeMenu} href={item.url} className='flex justify-center items-center text-black' key={item._key}>
                 <span className='flex text-lg items-center'>{item.title} &rarr;</span>
               </Link>
             ))}
           </li>
           <li className='order-1 lg:order-3 flex items-start justify-start pb-4 flex-col w-full space-y-4'>
-            <Link href="/kontakt" className="flex border border-green-dark bg-green-dark text-white w-full md:max-w-fit min-h-fit h-0 px-8 md:px-12 py-7 justify-between items-center rounded-full hover:bg-white hover:white-dark hover:text-black duration-300 ">
+            <Link onClick={closeMenu} href="/kontakt" className="flex border border-green-dark bg-green-dark text-white w-full md:max-w-fit min-h-fit h-0 px-8 md:px-12 py-7 justify-between items-center rounded-full hover:bg-white hover:white-dark hover:text-black duration-300 ">
               <span className='flex justify-between items-center w-full text-base'>
                 <span className='flex items-center justify-center'>Kontakt Oss</span>
                 &rarr;
